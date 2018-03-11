7a88bbc032f987f2d025bea9d6a904a7aecf0760
Rico Jones <rico@toasterlovin.com>
2018-03-11 12:11:13 -0700

    Add test stubs

--- test/column_test.rb
+++ test/column_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ColumnTest < Minitest::Test
+  def test_header_is_not_required
+    flunk
+  end
+
+  def test_column_header_is_case_insensitive_by_default
+    flunk
+  end
+
+  def test_column_header_ignores_whitespace_by_default
+    flunk
+  end
+
   def test_duplicate_columns
+    flunk 'should raise error at import time, not class definition time'
+
     assert_raises CSVParty::DuplicateColumnError do
       require 'importers/invalid/duplicate_columns_importer'
     end
